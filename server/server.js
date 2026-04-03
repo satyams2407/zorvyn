475b225d833232351c9c2dd3c7a6d1ed72e818dd
Satyam Singh <satyams2407@gmail.com>
2026-04-03 12:07:44 +0530

    added a src folder for a clean struc

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./src/routes/authRoutes');
+const userRoutes = require('./src/routes/userRoutes');
 
 const app = express();
 
